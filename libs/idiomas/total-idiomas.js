8968af89be5b6f68c3b44c33962db1b57bd3bfaa
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2023-12-23 22:04:22 -0300

    Add files via upload

--- libs/idiomas/total-idiomas.js
+++ libs/idiomas/total-idiomas.js
@@ -1,5 +1,9 @@
 const espanol = require('./espanol.js') //Español 
 const ingles = require('./ingles.js') //Ingles
+const arabe = require('./arabe.js') //عرب
+const indonesio = require('./indonesio.js') //Bahasa Indonesia
 
 module.exports.es = espanol
 module.exports.en = ingles
+module.exports.ar = arabe
+module.exports.id = indonesio
