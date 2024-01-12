a8049a296e93bae1148397b19277a30b92aec705
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-11 22:23:38 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,7 +33,8 @@ global.itsrose = ['4b146102c4d500809da9d1ff'];
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '');
 
 global.APIs = {
-  ApiEmpire: 'https://api-brunosobrino.zipponodes.xyz',
+  // ApiEmpire: 'https://',
+  CFROSAPI: 'https://api.cafirexos.com',
   xteam: 'https://api.xteam.xyz',
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
