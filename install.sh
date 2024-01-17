56852b6da7fa74c8c879a17f084bb4629ecdbafa
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-17 19:46:09 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -261,4 +261,5 @@ echo -e "\e[31m
     
 sleep 15 && pkill mpv > /dev/null 2>&1 && sleep 2 && rm -f /data/data/com.termux/files/home/"$random_mp3" &
 echo -e "\033[01;32m\033[01mＩＮＩＣＩＡＮＤＯ ＮＯＶＡＢＯＴ-ＭＤ 🚀\n\033[0m"
+npm start
 startBot()
